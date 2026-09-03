2a500eabc923a402da432542048e3fe485e4c0f6
obeidaalwardeooo6-debug <obeidaalwardeooo6@gmail.com>
2026-09-03 14:59:59 +0200

    feat: add synthetic events and failed login alert prototype

---
+++ app/collector/sample_events.py
@@ -0,0 +1,72 @@
+event1 = {
+    "timestamp": "2026-09-02T13:42:10",
+    "event_id": 4625,
+    "event_type": "failed_login",
+    "username": "demo_user",
+    "username": "demo_user",
+    "source_ip": "192.168.1.50",
+    "host": "LAB-PC-01",
+    "source": "windows_security"
+}
+
+event2 = {
+    "timestamp": "2026-09-02T13:42:20",
+    "event_id": 4625,
+    "event_type": "failed_login",
+    "username": "demo_user",
+    "username": "demo_user",
+    "source_ip": "192.168.1.50",
+    "host": "LAB-PC-01",
+    "source": "windows_security"
+}
+
+event3 = {
+    "timestamp": "2026-09-02T13:42:30",
+    "event_id": 4625,
+    "event_type": "failed_login",
+    "username": "demo_user",
+    "username": "demo_user",
+    "source_ip": "192.168.1.50",
+    "host": "LAB-PC-01",
+    "source": "windows_security"
+}
+
+event4 = {
+    "timestamp": "2026-09-02T13:42:35",
+    "event_id": 4625,
+    "event_type": "successful_login",
+    "username": "demo_user",
+    "source_ip": "192.168.1.50",
+    "host": "LAB-PC-01",
+    "source": "windows_security"
+}
+
+event5 = {
+    "timestamp": "2026-09-02T13:42:50",
+    "event_id": 4625,
+    "event_type": "failed_login",
+    "username": "demo_user",
+    "source_ip": "192.168.1.60",
+    "host": "LAB-PC-01",
+    "source": "windows_security"
+}
+
+event6 = {
+    "timestamp": "2026-09-02T13:43:00",
+    "event_id": 4625,
+    "event_type": "failed_login",
+    "username": "demo_user",
+    "source_ip": "192.168.1.60",
+    "host": "LAB-PC-01",
+    "source": "windows_security"
+}
+
+events = [
+    event1,
+    event2,
+    event3,
+    event4,
+    event5,
+    event6
+]
+
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from app.collector.sample_events import events
+
 print("Python SOC Monitor started")
+
+failed_count = 0
+
+for event in events:
+    print(event)
+
+    if event["event_type"] == "failed_login":
+        failed_count = failed_count + 1
+
+        if failed_count == 3:
+           print("ALERT: Multiple failed logins detected")
